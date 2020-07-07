67039963681883b5a43749eb47cf3931cbaae190
ashishkle <65059303+ashishkle@users.noreply.github.com>
2020-07-08 01:01:37 +0200

    retry
    retry

--- public/read.php
+++ public/read.php
@@ -16,12 +16,12 @@ if (isset($_POST['submit'])) {
     $connection = new PDO($dsn, $username, $password, $options);
 
     $sql = "SELECT * 
-            FROM users
-            WHERE location = :location";
+            FROM users ";
+            //WHERE location = :location";
 
-    $location = $_POST['location'];
+    //$location = $_POST['location'];
     $statement = $connection->prepare($sql);
-    $statement->bindParam(':location', $location, PDO::PARAM_STR);
+    //$statement->bindParam(':location', $location, PDO::PARAM_STR);
     $statement->execute();
 
     $result = $statement->fetchAll();
@@ -33,7 +33,6 @@ if (isset($_POST['submit'])) {
 <?php require "templates/header.php"; ?>
         
 <?php  
-if (isset($_POST['submit'])) {
   if ($result && $statement->rowCount() > 0) { ?>
     <h2>Results</h2>
 
@@ -65,19 +64,9 @@ if (isset($_POST['submit'])) {
       <?php endforeach; ?>
       </tbody>
     </table>
-    <?php } else { ?>
-      <blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
     <?php } 
 } ?> 
 
-<h2>Find user based on location</h2>
-
-<form method="post">
-  <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
-  <label for="location">Location</label>
-  <input type="text" id="location" name="location">
-  <input type="submit" name="submit" value="View Results">
-</form>
 
 <a href="index.php">Back to home</a>
 
