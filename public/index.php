bbd0b0b2f1eba07e2135d2c36040f429da0ed538
ashishkle <65059303+ashishkle@users.noreply.github.com>
2020-07-08 14:04:09 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 <ul>
 	<li><a href="create.php"><strong>Create a new tag</strong></a> - configure New tag</li>
 	<li><a href="read.php"><strong>List tags</strong></a> - find a user</li>
-	<li><a href="update.php"><strong>Update tags</strong></a> - edit a user</li>
-	<li><a href="delete.php"><strong>Delete tags</strong></a> - delete a user</li>
+	<li><a href="update.php"><strong>Update tags</strong></a> - edit a TAG</li>
+	<li><a href="delete.php"><strong>Delete tags</strong></a> - delete a TAG</li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
