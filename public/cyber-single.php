4cbe0107ee1e0d5809b8f0cadbd1bd5f3741f187
ashishkle <65059303+ashishkle@users.noreply.github.com>
2020-07-08 09:30:32 +0200

    Update cyber-single.php
    and again

--- public/cyber-single.php
+++ public/cyber-single.php
@@ -45,7 +45,7 @@ require "../common.php";
       </thead>
       <tbody>
       <?php foreach ($result as $row) : ?>
-        <tr style="line-height: 100px">
+        <tr style="height: 100px">
           <td><?php echo escape($row["id"]); ?></td>
           <td><?php echo escape($row["on2itcontext"]); ?></td>
           <td><?php echo escape($row["title"]); ?></td>
